d51130ef7c141b281e153796648935d608b85122
Michel Shintaku Martins <michel.martins@camara.leg.br>
2026-05-17 10:51:02 -0300

    Ajuste layout

--- tokens/compiled-version.js
+++ tokens/compiled-version.js
@@ -1,52 +1,44 @@
 var TOKEN_SEED = [
   {
-    "name": "text-color-base",
-    "type": "COLOR",
-    "modes": {
-      "light": "#000000",
-      "dark": "#FFFFFF"
-    }
-  },
-  {
     "name": "--alc-radius/none",
     "type": "NUMBER",
     "modes": {
-      "Mode 1": 0
+      "Radius": 0
     }
   },
   {
     "name": "--alc-radius/default",
     "type": "NUMBER",
     "modes": {
-      "Mode 1": 4
+      "Radius": 4
     }
   },
   {
     "name": "--alc-radius/md",
     "type": "NUMBER",
     "modes": {
-      "Mode 1": 8
+      "Radius": 8
     }
   },
   {
     "name": "--alc-radius/lg",
     "type": "NUMBER",
     "modes": {
-      "Mode 1": 12
+      "Radius": 12
     }
   },
   {
     "name": "--alc-radius/xl",
     "type": "NUMBER",
     "modes": {
-      "Mode 1": 16
+      "Radius": 16
     }
   },
   {
     "name": "--alc-radius/full",
     "type": "NUMBER",
     "modes": {
-      "Mode 1": 9999
+      "Radius": 9999
     }
   }
 ];
